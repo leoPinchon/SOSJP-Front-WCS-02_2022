9a056a370c5a41fb8c542a88ad5686a13455c950
Pinchon Léo <leopinchonspa@gmail.com>
2022-02-11 10:47:14 +0100

    initialisation du projet react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Wesh Alors !</h1>
+    </div>
+  );
+}
+
+export default App;
